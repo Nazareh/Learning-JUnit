48373fab1f6127e0b317088b3f5dabff56800974
Nazareh <nazarehturmina@gmail.com>
2018-03-08 11:35:00 +0800

    first commit

---
+++ iloveyouboss/1/test/iloveyouboss/ScoreCollectionTest.java
@@ -0,0 +1,28 @@
+package iloveyouboss;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class ScoreCollectionTest {
+
+    @Test
+    public void arithmeticMean() {
+        //fail("Not yet implemented");
+
+    }
+    @Test
+    public void answersArithmeticMeanOfTwoNumbers(){
+        //Arrange
+        ScoreCollection sc = new ScoreCollection();
+        sc.add(() -> 7 );
+        sc.add(() -> 5 );
+
+        //Act
+        int actualResult = sc.arithmeticMean();
+
+        //Assert
+        assertThat(actualResult,equalTo(9));
+
+    }
+}
