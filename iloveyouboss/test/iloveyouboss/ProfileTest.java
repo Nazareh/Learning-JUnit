0ef23a17dfdd3904bc76577c8ca2e3ba8d647542
Nazareh <nazarehturmina@gmail.com>
2018-03-09 10:57:32 +0800

    Add first @Before annotation

--- iloveyouboss/test/iloveyouboss/ProfileTest.java
+++ iloveyouboss/test/iloveyouboss/ProfileTest.java
@@ -1,48 +1,45 @@
 package iloveyouboss;
 
 import com.sun.org.apache.xpath.internal.operations.And;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ProfileTest {
+    private Profile profile;
+    private BooleanQuestion question;
+    private Criteria criteria;
+
+    @Before
+    public void create(){
+        question = new BooleanQuestion(1,"Got bonuses?");
+        profile = new Profile("Bull Hockey, Inc.");
+        criteria = new Criteria();
+
+    }
 
     @Test
+    //kill gets set to true because match is false and criterion.getWeight() equals Weight.MustMatch
     public void matchAnswersFalseWhenMustMatchCriteriaNotMet(){
         //Arrange
-        Question question = new BooleanQuestion(1,"Got bonuses?");
-
-        Profile profile = new Profile("Bull Hockey, Inc.");
         profile.add(new Answer(question,Bool.FALSE));
-
-        Criteria criteria = new Criteria();
-        Criterion criterion = new Criterion(new Answer(question,Bool.TRUE ),Weight.MustMatch);
-        criteria.add(criterion);
-
+        criteria.add(new Criterion(new Answer(question,Bool.TRUE ),Weight.MustMatch));
         //Act
         boolean matches = profile.matches(criteria);
-
         //Assert
         assertFalse(matches);
     }
 
     @Test
+    //match resolves to true because criterion.getWeight() returns Weight.DontCare.
     public void matchAnswersTrueForAnyDontCareCriteria(){
         //Arrange
-        Question question = new BooleanQuestion(1,"Got milk?");
-
-        Profile profile = new Profile("Bull Hockey, Inc.");
         profile.add(new Answer(question,Bool.FALSE));
-
-        Criteria criteria = new Criteria();
-        Criterion criterion = new Criterion(new Answer(question,Bool.TRUE ),Weight.DontCare);
-        criteria.add(criterion);
-
+        criteria.add(new Criterion(new Answer(question,Bool.TRUE ),Weight.DontCare));
         //Act
         boolean matches = profile.matches(criteria);
-
         //Assert
         assertTrue(matches);
-
     }
 }
--- iloveyouboss/test/iloveyouboss/ScoreCollectionTest.java
+++ iloveyouboss/test/iloveyouboss/ScoreCollectionTest.java
@@ -1,10 +1,12 @@
 package iloveyouboss;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
 public class ScoreCollectionTest {
+
 
     @Test
     public void arithmeticMean() {
